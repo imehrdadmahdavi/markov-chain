e68224da86e061afd0f8c0fbed6b93bd46c64214
Mehrdad Mahdavi <imehrdadmahdavi@gmail.com>
2018-07-08 14:52:19 -0700

    basic data retrieval functionality added

--- data_retrieval.py
+++ data_retrieval.py
@@ -1,3 +1,20 @@
 """
 Retrieve the text, store and clean data
 """
+import urllib2
+
+def retr():
+    base_url = "http://www.gutenberg.org/cache/epub/13060/pg13060.html"
+    file_name_raw = "gulistan-raw.txt"
+    response = urllib2.urlopen(base_url)
+    html = response.read()
+
+    words = html.split(' ')
+
+    print "Retrieving data from %s ..." % base_url
+    f = open(file_name_raw, "w+")
+    print "Writing raw data to %s ..." % file_name_raw
+    for item in words:
+        f.write(str(item) + "\n")
+    f.close()
+    print "Raw data was written in %s successfully!" % file_name_raw
--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 """
 Execute the Markov Chain of the data and produce the output
 """
+import data_retrieval
+from markov_python.cc_markov import MarkovChain
+
+data_retrieval.retr()

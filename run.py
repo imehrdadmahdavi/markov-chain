63164ea9e5a01b3b674e91e30d506ac623a92e16
Mehrdad Mahdavi <imehrdadmahdavi@gmail.com>
2018-07-08 15:21:41 -0700

    .pyc file deleted

--- run.py
+++ run.py
@@ -5,3 +5,9 @@ import data_retrieval
 from markov_python.cc_markov import MarkovChain
 
 data_retrieval.retr()
+mc = MarkovChain()
+mc.add_file("gulistan-raw.txt")
+sentence = mc.generate_text(20)
+output = ""
+for item in sentence:
+    print item,
